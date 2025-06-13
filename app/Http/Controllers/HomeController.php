6196c6f704588e8ae9b817fd59b79595da4f8151
Personal <faisaltez@gmail.com>
2025-06-14 01:13:26 +0600

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $projects = Project::where('status','=','active')->get();
-        return view('client.index', compact('projects'));
+        $projects = Project::where('status', '=', 'active')->get();
+        $sliders = Slider::where('status', '=', 'active')->get();
+
+        return view('client.index', compact('projects', 'sliders'));
     }
 }
